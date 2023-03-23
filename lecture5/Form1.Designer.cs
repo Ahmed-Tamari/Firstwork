855a3dec3b995392474e307240df7c65030ecebd
AHMEDTAMARI <ENG.AHMEDTAMARI@GMAIL.COM>
2023-03-23 04:18:58 +0300

    lecture 5

---
+++ lecture5/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace lecture5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUser = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnUser
+            // 
+            this.btnUser.Location = new System.Drawing.Point(85, 216);
+            this.btnUser.Name = "btnUser";
+            this.btnUser.Size = new System.Drawing.Size(206, 67);
+            this.btnUser.TabIndex = 0;
+            this.btnUser.Text = "Wellcome User";
+            this.btnUser.UseVisualStyleBackColor = true;
+            this.btnUser.Click += new System.EventHandler(this.btnUser_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(245, 162);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 1;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(107, 125);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(160, 20);
+            this.txt1.TabIndex = 2;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 326);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUser;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txt1;
+    }
+}
+
